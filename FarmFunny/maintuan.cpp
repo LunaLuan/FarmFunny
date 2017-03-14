aa03727f5c842b3539655721ac3e653a29d10d70
anhtuan1.nguyen <nguyenanhtuan200794@gmail.com>
2017-03-14 15:07:47 +0700

    no message

---
+++ FarmFunny/maintuan.cpp
@@ -0,0 +1,21 @@
+/*
+ * maintuan.cpp
+ *
+ *  Created on: Mar 14, 2017
+ *      Author: anhtuan1.nguyen
+ */
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 14, 2017
+ *      Author: anhtuan1.nguyen
+ */
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	cout<<"Hello";
+	return 0;
+}
