2349d33378eb427ff4ad10468889be94004a7ead
LGE\luan.ha <htluandc@gmail.com>
2017-03-14 16:51:43 +0700

    Animal

--- FarmFunny/Animal.cpp
+++ FarmFunny/Animal.cpp
@@ -12,6 +12,39 @@ Animal::Animal() {
 
 }
 
+string Animal::getName() {
+	return name;
+}
+
+void Animal::setName(string n) {
+	name = n;
+}
+
+int Animal::getAge() {
+	return age;
+}
+
+void Animal::setAge(int a) {
+	age = a;
+}
+
+float Animal::getWeight() {
+	return weight;
+}
+
+void Animal::setWeight(float w) {
+	weight = w;
+}
+
+int Animal::getPrice() {
+	return price;
+}
+
+void Animal::setPrice(int p) {
+	price = p;
+}
+
+
 Animal::~Animal() {
 	// TODO Auto-generated destructor stub
 }
--- FarmFunny/Animal.h
+++ FarmFunny/Animal.h
@@ -15,7 +15,25 @@ class Animal {
 public:
 	Animal();
 
+	string getName();
+	void setName(string n);
 
+	int getAge();
+	void setAge(int a);
+
+
+	float getWeight();
+	void setWeight(float w);
+
+	int getPrice();
+	void setPrice(int p);
+
+	virtual void sound() = 0;
+	virtual void eat() = 0;
+	virtual void goOut() = 0;
+	virtual void goBack() = 0;
+	virtual void die() = 0;
+	virtual void reproduce() = 0;
 
 	virtual ~Animal();
 
@@ -23,6 +41,7 @@ protected:
 	string name;
 	int age;
 	float weight;
+	int price;
 };
 
 #endif /* ANIMAL_H_ */
